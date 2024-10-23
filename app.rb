cf246f6635be6b4e91fad5df422ce431aedb9033
mzamoranoc <mzamoran@chicagobooth.edu>
2024-10-23 19:07:04 +0000

    app code

--- app.rb
+++ app.rb
@@ -1,9 +1,58 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader' if development?
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Route for Square calculation
+get '/square/new' do
+  erb :square
 end
+
+# Route to process the square result
+get '/square/results' do
+  @number = params[:number].to_f
+  @result = @number ** 2
+  erb :square_results
+end
+
+# Route for Square Root calculation
+get '/square_root/new' do
+  erb :square_root
+end
+
+# Route to process the square root result
+get '/square_root/results' do
+  @number = params[:number].to_f
+  @result = Math.sqrt(@number)
+  erb :square_root_results
+end
+
+# Route for Random Number calculation
+get '/random/new' do
+  erb :random
+end
+
+# Route to process random number result
+get '/random/results' do
+  @min = params[:user_min].to_f
+  @max = params[:user_max].to_f
+  @result = rand(@min..@max)
+  erb :random_results
+end
+
+# Route for Payment calculation
+get '/payment/new' do
+  erb :payment
+end
+
+# Route to process the payment result
+get '/payment/results' do
+  @apr = params[:apr].to_f / 100 / 12
+  @years = params[:years].to_i * 12
+  @present_value = params[:present_value].to_f
+
+  numerator = @apr * @present_value
+  denominator = 1 - (1 + @apr) ** -@years
+  @payment = numerator / denominator
+
+  erb :payment_results
+end
+
